d41a028fda22744babc7120c18cd4b95d731b514
Shepherd-bit <titusaoluoch@gmail.com>
2026-06-21 16:10:23 +0300

    Update ClaimWizard.jsx

--- src/components/ClaimWizard.jsx
+++ src/components/ClaimWizard.jsx
@@ -143,7 +143,6 @@ function ClaimWizard() {
         </div>
       </header>
 
-
       {/* Main Grid: Application Form Container vs Setup Workspace Help Sidebar */}
       <div style={styles.bodyGrid}>
         <main style={styles.formCard}>
